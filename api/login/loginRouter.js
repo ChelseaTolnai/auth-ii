e8619f02fb42edd08af54d2ce89b5cc2dc200341
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-05 09:26:02 -0500

    Setup of router files and folder structure.

---
+++ api/login/loginRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const loginRouter = require('./api/login/loginRouter');
+const registerRouter = require('./api/register/registerRouter');
+const usersRouter = require('./api/users/usersRouter');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use('/api/login', loginRouter);
+server.use('/api/register', registerRouter);
+server.use('/api/users', usersRouter);
 
 module.exports = server;
